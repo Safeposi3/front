8505e717e613ab105f123365a118ca5f7527713e
Carlos <andre021297@gmail.com>
2023-05-26 13:57:46 -0500

    optimized dashboard and add create reservations for sidebar

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import Main from "@/components/Main";
-import NavBar from "@/components/Nav";
 
+import dynamic from "next/dynamic";
+import "leaflet/dist/leaflet.css";
+import Sidebar from "../components/Sidebar";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+const { useRouter } = require("next/router");
+
+const MyMap = dynamic(() => import("../components/Map"), {
+  ssr: false,
+});
 export default function Dashboard() {
+  const [selectedPoint, setSelectedPoint] = useState(null);
+  const useAuth = useContext(UserContext);
+  const router = useRouter();
+
+  const handlePointClick = (point) => {
+    setSelectedPoint(point);
+  };
+
   return (
-    <div>
-      <Main />
+    <div className="flex flex-row lg:rounded-r-full">
+      <div className="w-[30%] bg-gray-200">
+        <Sidebar selectedPoint={selectedPoint} />
+      </div>
+      <div className="w-[70%]">
+        <MyMap onPointClick={handlePointClick} />
+      </div>
     </div>
   );
 }
