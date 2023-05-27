d0322a1bf9c3b1055742342d9a58f8f777e79ed1
AlfredoArizmende <k.arizmende@gmail.com>
2023-05-27 12:42:01 -0600

    LandingPage Responsive

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
 import React from "react";
 import Nav from "../components/Nav";
 import Image from "next/image";
-import { AiOutlineMail } from "react-icons/ai";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Nav />
-      <div className="w-full h-screen bg-[url('../public/mainBg.webp')] bg-right-top bg-no-repeat bg-cover">
-        <div className="max-w-[1240px] w-full h-full mx-auto pt-40 pb-20 grid grid-cols-2 gap-4 items-center">
-          <div className="bg-white p-20">
-            <h1>Penatibus cras elit montes habitasse</h1>
+      <div className="w-full bg-[url('../public/mainBg.webp')] bg-right-top bg-no-repeat bg-cover sm:h-screen">
+        <div className="max-w-[1240px] w-full h-full mx-auto grid grid-cols-1 gap-4 items-center sm:grid-cols-2 sm:pt-40 sm:pb-20">
+          <div className="bg-white p-5 pt-32 sm:pt-5 lg:p-20">
+            <h1 className="font-bold text-4xl">
+              Penatibus cras elit montes habitasse
+            </h1>
             <p className="my-6">
               empus nibh mattis duis vestibulum semper proin massa nam congue
               dis gravida. Nam; mattis platea ipsum. Mi sociis molestie suscipit
@@ -30,13 +28,13 @@ export default function Home() {
       </div>
 
       {/* about section */}
-      <div id="about" className="w-full md:h-screen flex items-center py-16">
+      <div className="w-full md:h-screen flex items-center py-16">
         <div className="max-w-[1240px] m-auto md:grid grid-cols-2 gap-24">
-          <div>
+          <div className="p-5">
             <p className="uppercase text-xl tracking-widest text-[#5651e5]">
               About
             </p>
-            <h2 className="py-4">Business</h2>
+            <h2 className="font-bold text-3xl py-4">Business</h2>
             <p className="py-2 text-gray-600">
               I specialize in building mobile responsive front-end UI
               applications that connect with API’s and other backend
@@ -64,15 +62,15 @@ export default function Home() {
       </div>
 
       {/* prices section */}
-      <div id="about" className="w-full flex items-center">
+      <div className="w-full flex flex-wrap-reverse items-center">
         <div className="m-auto md:grid grid-cols-2">
-          <div className="bg-[url('../public/prices.webp')] bg-center bg-no-repeat bg-cover"></div>
+          <div className="bg-[url('../public/prices.webp')] bg-center bg-no-repeat bg-cover h-60 sm:h-72 md:h-auto"></div>
 
-          <div className="bg-blue-400 p-24">
+          <div className="bg-[#cee4ea] p-5 lg:p-16">
             <p className="uppercase text-xl tracking-widest text-[#5651e5]">
               Precios
             </p>
-            <h2 className="py-4">¿Cuanto pagas?</h2>
+            <h2 className="font-bold text-3xl py-4">¿Cuanto pagas?</h2>
             <p className="py-2 text-gray-600">
               Date de alta en nuestra app y empieza a disfrutar de un servicio
               de coches con tarifa flexible por minutos, horas y días.
@@ -88,7 +86,7 @@ export default function Home() {
                 0,18€ - 0,36€*
               </div>
               <div className="border-t-2 flex justify-start items-center py-2">
-                segunda hora y consecutivas
+                precio por hora
               </div>
               <div className="border-t-2 flex justify-end items-center py-2 text-2xl font-bold">
                 9€*
@@ -107,24 +105,87 @@ export default function Home() {
         </div>
       </div>
 
-      {/* about section */}
-      <div id="about" className="w-full md:h-screen flex items-center py-16">
-        <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-4 bg-red-400">
-          <div className="bg-blue-500">
+      {/* Step section */}
+      <div className="w-full md:h-screen flex items-center py-16">
+        <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-20 p-5">
+          <div className="flex flex-col items-center justify-center">
             <Image
               src="/../public/coche.png"
               alt="/"
               width={150}
               height={150}
             />
-            <h2 className="py-4">empieza a conducir tu WiBLE en Madrid</h2>
+            <h2 className="font-bold text-3xl py-4">
+              empieza a conducir tu WiBLE en Madrid
+            </h2>
             <p className="py-2 text-gray-600">
               Empezar a conducir los coches híbridos enchufables WiBLE es muy
               fácil, completa estos pasos y ve más allá.
             </p>
           </div>
-          <div className="col-span-2 bg-yellow-500">
-            <div className="grid grid-cols-2"></div>
+          <div className="col-span-2">
+            <div className="grid grid-cols-2 gap-10">
+              <div className="flex flex-col">
+                <Image
+                  src="/../public/01.png"
+                  alt="/"
+                  width={90}
+                  height={115}
+                />
+                <p className="py-2 text-gray-600 font-bold">
+                  Descárgate la app de WiBLE
+                </p>
+                <p className="py-2 text-gray-600">
+                  Todos los servicios de WiBLE en tu movil. Encuentra tu WiBLE
+                  más cercano.
+                </p>
+              </div>
+              <div className="flex flex-col">
+                <Image
+                  src="/../public/01.png"
+                  alt="/"
+                  width={90}
+                  height={115}
+                />
+                <p className="py-2 text-gray-600 font-bold">
+                  Descárgate la app de WiBLE
+                </p>
+                <p className="py-2 text-gray-600">
+                  Todos los servicios de WiBLE en tu movil. Encuentra tu WiBLE
+                  más cercano.
+                </p>
+              </div>
+              <div className="flex flex-col">
+                <Image
+                  src="/../public/01.png"
+                  alt="/"
+                  width={90}
+                  height={115}
+                />
+                <p className="py-2 text-gray-600 font-bold">
+                  Descárgate la app de WiBLE
+                </p>
+                <p className="py-2 text-gray-600">
+                  Todos los servicios de WiBLE en tu movil. Encuentra tu WiBLE
+                  más cercano.
+                </p>
+              </div>
+              <div className="flex flex-col">
+                <Image
+                  src="/../public/01.png"
+                  alt="/"
+                  width={90}
+                  height={115}
+                />
+                <p className="py-2 text-gray-600 font-bold">
+                  Descárgate la app de WiBLE
+                </p>
+                <p className="py-2 text-gray-600">
+                  Todos los servicios de WiBLE en tu movil. Encuentra tu WiBLE
+                  más cercano.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
