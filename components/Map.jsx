e1834c5322465e9ac0052907f118cb35772d1eeb
AlfredoArizmende <k.arizmende@gmail.com>
2023-05-31 20:06:16 -0600

    Dashboard responsive

--- components/Map.jsx
+++ components/Map.jsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 import { Icon } from "leaflet";
 import { useDispatch, useSelector } from "react-redux";
 import { listBuoys } from "@/redux/actions/buoys";
+
 export default function Map({ onPointClick, selectedShip }) {
   const dispatch = useDispatch();
   const { loading, data, error } = useSelector((state) => state.listBuoys);
 
   const [selectedBuoy, setSelectedBuoy] = useState(null);
   const [tabarcaCoords, setTabarcaCoords] = useState([
-    38.16631881567097,
-    -0.4794345305520023,
+    38.16631881567097, -0.4794345305520023,
   ]);
 
   useEffect(() => {
@@ -31,16 +31,18 @@ export default function Map({ onPointClick, selectedShip }) {
     iconUrl: "/red-dot.png",
     iconSize: [12, 12],
   });
+
   return (
     <div
-      className="border border-gray-300 rounded-md"
-      style={{ height: "90vh", width: "70vw" }}
+      className="w-[73vw] h-[65vh] sm:w-[77.5vw] md:w-[81.5vw] lg:w-[63vw] xl:w-[67vw]"
+      // style={{ height: "90vh", width: "70vw" }}
       //with this you can modify the size of the map vh is equal to % of viewport
     >
       <MapContainer
         center={tabarcaCoords}
         zoom={17}
-        style={{ height: "100%", width: "100%" }}
+        // style={{ height: "100%", width: "100%" }}
+        className="w-[100%] h-[100%]"
         scrollWheelZoom={false}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
